c10472b82ed92923be84badc6dd555c6814ea9ea
LEE-JAE-HYUN179 <leejaehyun1223@gmail.com>
2019-12-12 01:35:43 +0900

    finish

--- trainer.py
+++ trainer.py
@@ -11,7 +11,6 @@ import torch.nn as nn
 import torch.optim as optim
 import torch
 import torch.nn.functional as F
-from sampler import ImbalancedDatasetSampler
 
 '''
 __init__ 에 optimizer 에 따른 if 문
@@ -22,14 +21,14 @@ validation_dir = './preprocessed_data/validate'
 age_tensor = torch.tensor([i for i in range(1, 101)]).type(torch.FloatTensor).to(device)
 
 data_transforms = {
-    'train': transforms.Compose([transforms.Resize(255),
-                                 transforms.CenterCrop(224),
-                                 transforms.RandomHorizontalFlip(),
+    'train': transforms.Compose([transforms.Resize(224),
+                                 #transforms.RandomHorizontalFlip(),
+                                 #transforms.RandomRotation(10),
+                                 #transforms.ColorJitter(0.4, 0.4, 0.4),
                                  transforms.ToTensor(),
                                  transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # 의미?
                                  ]),
-    'val': transforms.Compose([transforms.Resize(255),
-                               transforms.CenterCrop(224),
+    'val': transforms.Compose([transforms.Resize(224),
                                transforms.ToTensor(),
                                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # 의미?
                                ]),
@@ -37,25 +36,30 @@ data_transforms = {
 
 
 class Trainer:
-    def __init__(self, model):
+    def __init__(self, model, optimizer_name='Adam', lr=0.0002, epoch=65, batch_size=256,
+                 weight_decay=0.001, momentum=0.9,
+                 nes=True, lr_decay=(50, 0.1)):
         self.model = model
-        self.set_hyperparameter()
-        self.set_optimizer()
+        self.lr = lr
+        self.batch_size = batch_size
+        self.epoch = epoch
+        self.weight_decay = weight_decay
+        self.momentum = momentum
+        self.nes = nes
+        self.set_optimizer(optimizer_name)
+        self.scheculer = optim.lr_scheduler.StepLR(self.optimizer, lr_decay[0], lr_decay[1])
         self.set_loss()
-        self.set_lr_schedule()
 
-    def set_model(self, model):
-        self.model = model
-
-    def set_optimizer(self, optimizer_name='Adam'):
+    def set_optimizer(self, optimizer_name):
         if optimizer_name == 'Adam':
-            self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr, weight_decay=self.weight_dacay)
+            self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr, weight_decay=self.weight_decay)
         elif optimizer_name == 'SGD':
-            self.optimizer = optim.SGD(self.model.parameters(), lr=self.lr, weight_decay=self.weight_dacay)
+            self.optimizer = optim.SGD(self.model.parameters(), lr=self.lr, weight_decay=self.weight_decay,
+                                       momentum=self.momentum, nesterov=self.nes)
         elif optimizer_name == 'RMSprop':
-            self.optimizer = optim.RMSprop(self.model.parameters(), lr=self.lr, weight_decay=self.weight_dacay)
+            self.optimizer = optim.RMSprop(self.model.parameters(), lr=self.lr, weight_decay=self.weight_decay)
         elif optimizer_name == 'AdaGrad':
-            self.optimizer = optim.Adagrad(self.model.parameters(), lr=self.lr, weight_decay=self.weight_dacay)
+            self.optimizer = optim.Adagrad(self.model.parameters(), lr=self.lr, weight_decay=self.weight_decay)
         elif optimizer_name == 'Adadelta':
             self.optimizer = optim.Adadelta(self.model.parameters(), lr=self.lr, weight_decay=self.weight_dacay)
         else:
@@ -71,15 +75,6 @@ class Trainer:
         else:
             raise ModuleNotFoundError
 
-    def set_hyperparameter(self, lr=0.0001, batch_size=128, epoch=60, weight_decay=0):  # 조정!: 128,50?
-        self.lr = lr
-        self.batch_size = batch_size
-        self.epoch = epoch
-        self.weight_dacay = weight_decay
-
-    def set_lr_schedule(self, step_size=10, gamma=0.1):
-        self.scheculer = optim.lr_scheduler.StepLR(self.optimizer, step_size=step_size, gamma=gamma)
-
     def train(self):
 
         self.model.to(device)
@@ -93,6 +88,7 @@ class Trainer:
 
         val_loss = 999
         threshold = 0
+        patient = 0
 
         for i in range(self.epoch):
             self.model.train()
@@ -110,28 +106,28 @@ class Trainer:
                 train_loss.append(loss.item())
                 print('Real time loss: ', loss)
                 print('Training Percesnt : --------{}%--------'.format(
-                    100 * (self.batch_size * j + 20000 * i) / (self.epoch * 20000)))
+                    100 * (self.batch_size * j + 45000 * i) / (self.epoch * 45000)))
+            iter += 1
 
             writer.add_scalar('Loss/train', sum(train_loss) / len(train_loss), iter)
-            iter += 1
             self.scheculer.step()
             val_loss_next = self.validate()
             writer.add_scalar('Loss/Validation', val_loss_next, iter)
-            iter += 1
 
             if val_loss_next > val_loss:
-                print('Early stopping')
-                break
-            elif val_loss - val_loss_next < 0.05:
-                threshold += 1
+                patient += 1
             else:
-                threshold = 0
+                patient = 0
+                if val_loss - val_loss_next < 0.01:
+                    threshold += 1
+                else:
+                    threshold = 0
 
             val_loss = val_loss_next
 
             print(threshold)
 
-            if threshold >= 2:
+            if threshold >= 15 or patient >= 5:
                 break
 
     def validate(self):
@@ -186,29 +182,11 @@ def train_models():
 
     inceptionv3_path = './trained_model/inceptionv3.pt'
 
-    densenet121_model = DenseNet.get_densenet121()
-    model_trainer = Trainer(densenet121_model)
-    model_trainer.set_hyperparameter(batch_size=36, lr=0.0015)
-    model_trainer.set_lr_schedule(5, 0.34)
+    resnet18_model = Resnet.get_resnet18()
+    model_trainer = Trainer(resnet18_model)
     model_trainer.train()
-    torch.save(densenet121_model.state_dict(), densenet121_path)
-    del densenet121_model, model_trainer
-
-    densenet121_model = DenseNet.get_densenet121()
-    model_trainer = Trainer(densenet121_model)
-    model_trainer.set_hyperparameter(batch_size=36, lr=0.0015)
-    model_trainer.set_lr_schedule(3, 0.45)
-    model_trainer.train()
-    torch.save(densenet121_model.state_dict(), densenet121_path)
-    del densenet121_model, model_trainer
-
-    densenet161_model = DenseNet.get_densenet161()
-    model_trainer = Trainer(densenet161_model)
-    model_trainer.set_hyperparameter(batch_size=20, lr=0.0001)
-    model_trainer.set_lr_schedule(3, 0.6)
-    model_trainer.train()
-    torch.save(densenet161_model.state_dict(), densenet161_path)
-    del densenet161_model, model_trainer
+    torch.save(resnet18_model.state_dict(), res18_path)
+    del resnet18_model, model_trainer
 
 
 if __name__ == '__main__':
